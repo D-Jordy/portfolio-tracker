30ff91f67df4aa325c769f5d05758f974216d0ef
D-Jordy <kaaljordy@gmail.com>
2026-06-03 14:43:44 +0200

    Add DividendController, /dividends route, and nav links
    Wire ComputeIncomingDividends into a controller and register the
    /dividends route in the auth group. Add Dividends nav links to both
    desktop and mobile navigation. Also fix mobile nav which was still
    linking to the deprecated /dashboard route instead of /portfolio.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -40,6 +40,12 @@ const showingNavigationDropdown = ref(false);
                                     Portfolio
                                 </NavLink>
                                 <NavLink
+                                    :href="route('dividends')"
+                                    :active="route().current('dividends')"
+                                >
+                                    Dividends
+                                </NavLink>
+                                <NavLink
                                     :href="route('accounts.index')"
                                     :active="route().current('accounts.*')"
                                 >
@@ -147,10 +153,16 @@ const showingNavigationDropdown = ref(false);
                 >
                     <div class="space-y-1 pb-3 pt-2">
                         <ResponsiveNavLink
-                            :href="route('dashboard')"
-                            :active="route().current('dashboard')"
+                            :href="route('portfolio')"
+                            :active="route().current('portfolio')"
                         >
-                            Dashboard
+                            Portfolio
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            :href="route('dividends')"
+                            :active="route().current('dividends')"
+                        >
+                            Dividends
                         </ResponsiveNavLink>
                         <ResponsiveNavLink
                             :href="route('accounts.index')"
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\DividendController;
 use App\Http\Controllers\ImportController;
 use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\ProfileController;
@@ -26,6 +27,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     // Portfolio
     Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
+
+    // Dividend income forecast
+    Route::get('/dividends', [DividendController::class, 'index'])->name('dividends');
 
     // Accounts
     Route::get('/accounts', [AccountController::class, 'index'])->name('accounts.index');
